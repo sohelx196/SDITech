9d2d4afe8d67b36618395ac1ac72e8bf3814dbd6
sohelx196 <sohel.devloper196@gmail.com>
2025-08-17 10:03:52 +0530

    redirect addded

--- src/component/LostAndFound.jsx
+++ src/component/LostAndFound.jsx
@@ -5,11 +5,13 @@ import comingSoon from '../assets/logos/comingSoon.png'
 function LostAndFound() {
   return (
     <div>
+      
           <ComingSoon
             ComingSoonImg={comingSoon}
             text1 = "Lost & Found Coming"
             text2 = "Soon!"
           />
+
           <p className='text-gray-400 text-center text-xl'>Every lost item carries someone’s story, a memory, a need, or a daily essential. By returning it, you’re not just giving back an object, you’re restoring someone’s smile.</p>
           <p className='text-gray-400 text-center text-xl'>Be a reason of someone’s smile.</p>
     </div>
